1530d9a093918d0ae34566f2e487291a733015aa
Deeksha Sharma <sharmadeeksha325@gmail.com>
2021-01-03 22:54:31 +0530

    Changed style

--- js/scripts.js
+++ js/scripts.js
@@ -34,32 +34,32 @@ var light = {
     y: 200
 }
 
-var colors = ["#f5c156", "#e6616b", "#5cd3ad"];
+var colors = ["#fff"];
 
 function drawLight() {
     ctx.beginPath();
     ctx.arc(light.x, light.y, 1000, 0, 2 * Math.PI);
     var gradient = ctx.createRadialGradient(light.x, light.y, 0, light.x, light.y, 1000);
-    gradient.addColorStop(0, "#3b4654");
-    gradient.addColorStop(1, "#2c343f");
+    // gradient.addColorStop(0, "#3b4654");
+    // gradient.addColorStop(1, "#2c343f");
     ctx.fillStyle = gradient;
     ctx.fill();
 
     ctx.beginPath();
     ctx.arc(light.x, light.y, 20, 0, 2 * Math.PI);
     gradient = ctx.createRadialGradient(light.x, light.y, 0, light.x, light.y, 5);
-    gradient.addColorStop(0, "#fff");
-    gradient.addColorStop(1, "#3b4654");
+    // gradient.addColorStop(0, "#fff");
+    // gradient.addColorStop(1, "#3b4654");
     ctx.fillStyle = gradient;
     ctx.fill();
 }
 
 function Box() {
-    this.half_size = Math.floor((Math.random() * 50) + 1);
+    this.half_size = Math.floor((Math.random() * 5) + 1);
     this.x = Math.floor((Math.random() * c.width) + 1);
     this.y = Math.floor((Math.random() * c.height) + 1);
     this.r = Math.random() * Math.PI;
-    this.shadow_length = 2000;
+    this.shadow_length = 0;
     this.color = colors[Math.floor((Math.random() * colors.length))];
   
     this.getDots = function() {
@@ -92,7 +92,7 @@ function Box() {
         };
     }
     this.rotate = function() {
-        var speed = (60 - this.half_size) / 20;
+        var speed = (30 - this.half_size) / 20;
         this.r += speed * 0.002;
         this.x += speed;
         this.y += speed;
@@ -166,7 +166,7 @@ function draw() {
 resize();
 draw();
 
-while (boxes.length < 14) {
+while (boxes.length < 40) {
     boxes.push(new Box());
 }
 
