af85ae786aa1cba551347ee87e3bb0ea95d059f8
Anshit01 <bhardwaj.anshit1379@gmail.com>
2021-01-19 12:51:05 +0530

    updated model

--- teamEXEWebsite/app/models.py
+++ teamEXEWebsite/app/models.py
@@ -19,8 +19,11 @@ positions = (
 
 class Member(models.Model):
     name = models.CharField(max_length=100)
-    position = models.CharField(max_length=100, choices=positions)
+    position = models.CharField(max_length=100, choices=positions, db_index=True)
     tagline = models.CharField(max_length=300)
     imageURL = models.URLField(max_length=200)
     github = models.URLField(max_length=200)
     linkedin = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.position + ' - ' + self.name
