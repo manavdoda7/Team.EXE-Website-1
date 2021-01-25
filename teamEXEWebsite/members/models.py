0839a5fa776ceb32cee7109f1426dc16ff16e4fc
Anshit01 <bhardwaj.anshit1379@gmail.com>
2021-01-26 03:13:26 +0530

    members pages fixed and activities pages made

--- teamEXEWebsite/members/models.py
+++ teamEXEWebsite/members/models.py
@@ -3,10 +3,9 @@ from django.db import models
 # Create your models here.
 
 positions = (
-    ('', ''),
+    ('.', '.'),
     ('Coordinator', 'Coordinator'),
     ('Executive Member', 'Executive Member'),
-    ('Alumnus', 'Alumnus'),
     ('Volunteer', 'Volunteer')
 )
 
@@ -21,7 +20,7 @@ years = (
 class Member(models.Model):
     name = models.CharField(max_length=100)
     year = models.IntegerField(choices=years, db_index=True)
-    position = models.CharField(max_length=100, choices=positions, db_index=True)
+    position = models.CharField(max_length=100, choices=positions, default='.', db_index=True)
     tagline = models.CharField(max_length=300)
     imageURL = models.URLField(max_length=200)
     github = models.URLField(max_length=200)
@@ -29,3 +28,12 @@ class Member(models.Model):
 
     def __str__(self):
         return str(self.year) + ' - ' + self.position + ' - ' + self.name
+
+class Alumnus(models.Model):
+    name = models.CharField(max_length=100)
+    imageURL = models.URLField(max_length=200)
+    github = models.URLField(max_length=200)
+    linkedin = models.URLField(max_length=200)
+
+    def __str__(self):
+        return self.name
--- teamEXEWebsite/members/views.py
+++ teamEXEWebsite/members/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Member
+from .models import Member, Alumnus
 
 # Create your views here.
 
 def members(request):
     context = {
         'coordinators' : Member.objects.filter(position='Coordinator'),
-        'executiveMembers' : Member.objects.filter(position='Executive Member')
+        'finalYear' : Member.objects.filter(year=4),
+        'thirdYear' : Member.objects.filter(year=3).exclude(position='Coordinator'),
+        'secondYear' : Member.objects.filter(year=2),
+        'firstYear' : Member.objects.filter(year=1)
     }
     return render(request, 'members.html', context)
 
 def alumni(request):
     context = {
-        'alumni' : Member.objects.filter(position='Alumnus')
+        'alumni' : Alumnus.objects.all()
     }
     return render(request, 'alumni.html', context)
