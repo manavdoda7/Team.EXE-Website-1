4e99852389767f748c1775118a3c56f0c3db9ac9
Raman Preet Singh <ramanpre0810@gmail.com>
2021-01-29 00:31:46 +0530

    Updated carasol nav and added the events model

--- teamEXEWebsite/activities/models.py
+++ teamEXEWebsite/activities/models.py
@@ -27,8 +27,8 @@ class Workshop(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(max_length=1000)
     time = models.DateTimeField()
-    link = models.URLField(max_length=255)
     image = models.URLField(max_length=255)
+    link = models.URLField(max_length=255)
 
     def __str__(self):
         return self.title
