6cdfbe12d8317abf785739f0430dcfd74157f4c6
shwetalsoni <sonishwetal704@gmail.com>
2021-01-28 16:31:53 +0530

    .

--- teamEXEWebsite/members/static/js/members.js
+++ teamEXEWebsite/members/static/js/members.js
@@ -7,30 +7,39 @@ $(document).ready(function () {
   
     // If the screen is smaller then 840px wide remove all classes.
     if ($(window).width() < breakpoint) {
-      $('.js-slidein').removeClass('js-slidein')
+        $('.js-slidein').removeClass('js-slidein')
     }
   
     // Check which of the elements with this class is visible on the page
     $('.js-slidein').each(function (i) {
-      var bottomObject = $(this).offset().top
-      var bottomWindow = $(window).scrollTop() + $(window).height()
-  
-      if (bottomWindow > bottomObject) {
-        $(this).addClass('js-slidein-visible')
-      }
+        var bottomObject = $(this).offset().top
+        var bottomWindow = $(window).scrollTop() + $(window).height()
+    
+        if (bottomWindow > bottomObject) {
+            $(this).addClass('js-slidein-visible')
+        }
     })
   
     // Trigger the slide-in effect on scroll
     $(window).scroll(function () {
-      $('.js-slidein').each(function (i) {
-        var bottomObject = $(this).offset().top + $(this).outerHeight() / 3
-        var bottomWindow = $(window).scrollTop() + $(window).height()
-  
-        if (bottomWindow > bottomObject) {
-          $(this).addClass('js-slidein-visible')
-        }else {
-            $(this).removeClass('js-slidein-visible')
-        }
-      })
+        let delay = 0
+        $('.js-slidein').each(function (i) {
+            var bottomObject = $(this).offset().top + $(this).outerHeight() / 3
+            var bottomWindow = $(window).scrollTop() + $(window).height()
+    
+            if (bottomWindow > bottomObject) {
+                if($(this).hasClass('js-slidein-visible') == false && $(this).hasClass('js-slidein-delay') == false){
+                    $(this).addClass('js-slidein-delay')
+                        setTimeout(() => {
+                        console.log(delay)
+                        $(this).addClass('js-slidein-visible')
+                        $(this).removeClass('js-slidein-delay')
+                    }, delay)
+                    delay += 100
+                }
+            }else {
+                $(this).removeClass('js-slidein-visible')
+            }
+        })
     })
 })
